44081fcee3e649cef0649cdf6df0bdeaf4f9445e
4-13 <danilmatvienko@gmail.com>
2023-05-01 13:21:14 +0300

    refactor: replace {lang}.json to {lang}.js

---
+++ src/keyboard_languages/en.js
@@ -0,0 +1,353 @@
+const en = {
+  name: 'English',
+  shortName: 'Eng',
+  keys: {
+    Backquote: {
+      defaultValue: '`',
+      alternateValue: '~',
+      type: 'char',
+    },
+    Digit1: {
+      defaultValue: '1',
+      alternateValue: '!',
+      type: 'char',
+    },
+    Digit2: {
+      defaultValue: '2',
+      alternateValue: '@',
+      type: 'char',
+    },
+    Digit3: {
+      defaultValue: '3',
+      alternateValue: '#',
+      type: 'char',
+    },
+    Digit4: {
+      defaultValue: '4',
+      alternateValue: '$',
+      type: 'char',
+    },
+    Digit5: {
+      defaultValue: '5',
+      alternateValue: '%',
+      type: 'char',
+    },
+    Digit6: {
+      defaultValue: '6',
+      alternateValue: '^',
+      type: 'char',
+    },
+    Digit7: {
+      defaultValue: '7',
+      alternateValue: '&',
+      type: 'char',
+    },
+    Digit8: {
+      defaultValue: '8',
+      alternateValue: '*',
+      type: 'char',
+    },
+    Digit9: {
+      defaultValue: '9',
+      alternateValue: '(',
+      type: 'char',
+    },
+    Digit0: {
+      defaultValue: '0',
+      alternateValue: ')',
+      type: 'char',
+    },
+    Minus: {
+      defaultValue: '-',
+      alternateValue: '_',
+      type: 'char',
+    },
+    Equal: {
+      defaultValue: '=',
+      alternateValue: '+',
+      type: 'char',
+    },
+    Backspace: {
+      defaultValue: '⟵',
+      alternateValue: '⟵',
+      type: 'control',
+    },
+    Tab: {
+      defaultValue: 'Tab',
+      alternateValue: 'Tab',
+      type: 'control',
+    },
+    KeyQ: {
+      defaultValue: 'q',
+      alternateValue: 'Q',
+      type: 'letter',
+    },
+    KeyW: {
+      defaultValue: 'w',
+      alternateValue: 'W',
+      type: 'letter',
+    },
+    KeyE: {
+      defaultValue: 'e',
+      alternateValue: 'E',
+      type: 'letter',
+    },
+    KeyR: {
+      defaultValue: 'r',
+      alternateValue: 'R',
+      type: 'letter',
+    },
+    KeyT: {
+      defaultValue: 't',
+      alternateValue: 'T',
+      type: 'letter',
+    },
+    KeyY: {
+      defaultValue: 'y',
+      alternateValue: 'Y',
+      type: 'letter',
+    },
+    KeyU: {
+      defaultValue: 'u',
+      alternateValue: 'U',
+      type: 'letter',
+    },
+    KeyI: {
+      defaultValue: 'i',
+      alternateValue: 'I',
+      type: 'letter',
+    },
+    KeyO: {
+      defaultValue: 'o',
+      alternateValue: 'O',
+      type: 'letter',
+    },
+    KeyP: {
+      defaultValue: 'p',
+      alternateValue: 'P',
+      type: 'letter',
+    },
+    BracketLeft: {
+      defaultValue: '[',
+      alternateValue: '{',
+      type: 'char',
+    },
+    BracketRight: {
+      defaultValue: ']',
+      alternateValue: '}',
+      type: 'char',
+    },
+    CapsLock: {
+      defaultValue: 'Caps Lock',
+      alternateValue: 'Caps Lock',
+      type: 'control',
+    },
+    KeyA: {
+      defaultValue: 'a',
+      alternateValue: 'A',
+      type: 'letter',
+    },
+    KeyS: {
+      defaultValue: 's',
+      alternateValue: 'S',
+      type: 'letter',
+    },
+    KeyD: {
+      defaultValue: 'd',
+      alternateValue: 'D',
+      type: 'letter',
+    },
+    KeyF: {
+      defaultValue: 'f',
+      alternateValue: 'F',
+      type: 'letter',
+    },
+    KeyG: {
+      defaultValue: 'g',
+      alternateValue: 'G',
+      type: 'letter',
+    },
+    KeyH: {
+      defaultValue: 'h',
+      alternateValue: 'H',
+      type: 'letter',
+    },
+    KeyJ: {
+      defaultValue: 'j',
+      alternateValue: 'J',
+      type: 'letter',
+    },
+    KeyK: {
+      defaultValue: 'k',
+      alternateValue: 'K',
+      type: 'letter',
+    },
+    KeyL: {
+      defaultValue: 'l',
+      alternateValue: 'L',
+      type: 'letter',
+    },
+    Semicolon: {
+      defaultValue: ';',
+      alternateValue: ':',
+      type: 'char',
+    },
+    Quote: {
+      defaultValue: "'",
+      alternateValue: '"',
+      type: 'char',
+    },
+    Backslash: {
+      defaultValue: '\\',
+      alternateValue: '|',
+      type: 'char',
+    },
+    Enter: {
+      defaultValue: 'Enter',
+      alternateValue: 'Enter',
+      type: 'control',
+    },
+    ShiftLeft: {
+      defaultValue: 'Shift',
+      alternateValue: 'Shift',
+      type: 'control',
+    },
+    IntlBackslash: {
+      defaultValue: '\\',
+      alternateValue: '|',
+      type: 'char',
+    },
+    KeyZ: {
+      defaultValue: 'z',
+      alternateValue: 'Z',
+      type: 'letter',
+    },
+    KeyX: {
+      defaultValue: 'x',
+      alternateValue: 'X',
+      type: 'letter',
+    },
+    KeyC: {
+      defaultValue: 'c',
+      alternateValue: 'C',
+      type: 'letter',
+    },
+    KeyV: {
+      defaultValue: 'v',
+      alternateValue: 'V',
+      type: 'letter',
+    },
+    KeyB: {
+      defaultValue: 'b',
+      alternateValue: 'B',
+      type: 'letter',
+    },
+    KeyN: {
+      defaultValue: 'n',
+      alternateValue: 'N',
+      type: 'letter',
+    },
+    KeyM: {
+      defaultValue: 'm',
+      alternateValue: 'M',
+      type: 'letter',
+    },
+    Comma: {
+      defaultValue: ',',
+      alternateValue: '<',
+      type: 'char',
+    },
+    Period: {
+      defaultValue: '.',
+      alternateValue: '>',
+      type: 'char',
+    },
+    Slash: {
+      defaultValue: '/',
+      alternateValue: '?',
+      type: 'char',
+    },
+    ShiftRight: {
+      defaultValue: 'Shift',
+      alternateValue: 'Shift',
+      type: 'control',
+    },
+    ControlLeft: {
+      defaultValue: 'Ctrl',
+      alternateValue: 'Ctrl',
+      type: 'control',
+    },
+    MetaLeft: {
+      defaultValue: 'Win',
+      alternateValue: 'Win',
+      type: 'control',
+    },
+    AltLeft: {
+      defaultValue: 'Alt',
+      alternateValue: 'Alt',
+      type: 'control',
+    },
+    Space: {
+      defaultValue: ' ',
+      alternateValue: ' ',
+      type: 'char',
+    },
+    AltRight: {
+      defaultValue: 'AltGr',
+      alternateValue: 'AltGr',
+      type: 'control',
+    },
+    MetaRight: {
+      defaultValue: 'Win',
+      alternateValue: 'Win',
+      type: 'control',
+    },
+    ContextMenu: {
+      defaultValue: 'ContextMenu',
+      alternateValue: 'ContextMenu',
+      type: 'control',
+    },
+    ControlRight: {
+      defaultValue: 'Ctrl',
+      alternateValue: 'Ctrl',
+      type: 'control',
+    },
+    Escape: {
+      defaultValue: 'Escape',
+      alternateValue: 'Escape',
+      type: 'control',
+    },
+    ArrowUp: {
+      defaultValue: '🠙',
+      alternateValue: '🠙',
+      type: 'cursor',
+    },
+    ArrowDown: {
+      defaultValue: '🠛',
+      alternateValue: '🠛',
+      type: 'cursor',
+    },
+    ArrowLeft: {
+      defaultValue: '🠘',
+      alternateValue: '🠘',
+      type: 'cursor',
+    },
+    ArrowRight: {
+      defaultValue: '🠚',
+      alternateValue: '🠚',
+      type: 'cursor',
+    },
+    Delete: {
+      defaultValue: 'Del',
+      alternateValue: 'Del',
+      type: 'control',
+    },
+    LangSwitch: {
+      defaultValue: '🌐',
+      alternateValue: '🌐',
+      type: 'control',
+    },
+  },
+};
+
+export default en;
---
+++ src/keyboard_languages/uk.js
@@ -0,0 +1,353 @@
+const uk = {
+  name: 'Українська',
+  shortName: 'Укр',
+  keys: {
+    Backquote: {
+      defaultValue: "'",
+      alternateValue: '₴',
+      type: 'char',
+    },
+    Digit1: {
+      defaultValue: '1',
+      alternateValue: '!',
+      type: 'char',
+    },
+    Digit2: {
+      defaultValue: '2',
+      alternateValue: '"',
+      type: 'char',
+    },
+    Digit3: {
+      defaultValue: '3',
+      alternateValue: '№',
+      type: 'char',
+    },
+    Digit4: {
+      defaultValue: '4',
+      alternateValue: ';',
+      type: 'char',
+    },
+    Digit5: {
+      defaultValue: '5',
+      alternateValue: '%',
+      type: 'char',
+    },
+    Digit6: {
+      defaultValue: '6',
+      alternateValue: ':',
+      type: 'char',
+    },
+    Digit7: {
+      defaultValue: '7',
+      alternateValue: '?',
+      type: 'char',
+    },
+    Digit8: {
+      defaultValue: '8',
+      alternateValue: '*',
+      type: 'char',
+    },
+    Digit9: {
+      defaultValue: '9',
+      alternateValue: '(',
+      type: 'char',
+    },
+    Digit0: {
+      defaultValue: '0',
+      alternateValue: ')',
+      type: 'char',
+    },
+    Minus: {
+      defaultValue: '-',
+      alternateValue: '_',
+      type: 'char',
+    },
+    Equal: {
+      defaultValue: '=',
+      alternateValue: '+',
+      type: 'char',
+    },
+    Backspace: {
+      defaultValue: '⟵',
+      alternateValue: '⟵',
+      type: 'control',
+    },
+    Tab: {
+      defaultValue: 'Tab',
+      alternateValue: 'Tab',
+      type: 'control',
+    },
+    KeyQ: {
+      defaultValue: 'й',
+      alternateValue: 'Й',
+      type: 'letter',
+    },
+    KeyW: {
+      defaultValue: 'ц',
+      alternateValue: 'Ц',
+      type: 'letter',
+    },
+    KeyE: {
+      defaultValue: 'у',
+      alternateValue: 'У',
+      type: 'letter',
+    },
+    KeyR: {
+      defaultValue: 'к',
+      alternateValue: 'К',
+      type: 'letter',
+    },
+    KeyT: {
+      defaultValue: 'е',
+      alternateValue: 'Е',
+      type: 'letter',
+    },
+    KeyY: {
+      defaultValue: 'н',
+      alternateValue: 'Н',
+      type: 'letter',
+    },
+    KeyU: {
+      defaultValue: 'г',
+      alternateValue: 'Г',
+      type: 'letter',
+    },
+    KeyI: {
+      defaultValue: 'ш',
+      alternateValue: 'Ш',
+      type: 'letter',
+    },
+    KeyO: {
+      defaultValue: 'щ',
+      alternateValue: 'Щ',
+      type: 'letter',
+    },
+    KeyP: {
+      defaultValue: 'з',
+      alternateValue: 'З',
+      type: 'letter',
+    },
+    BracketLeft: {
+      defaultValue: 'х',
+      alternateValue: 'Х',
+      type: 'letter',
+    },
+    BracketRight: {
+      defaultValue: 'ї',
+      alternateValue: 'Ї',
+      type: 'letter',
+    },
+    Enter: {
+      defaultValue: 'Enter',
+      alternateValue: 'Enter',
+      type: 'control',
+    },
+    CapsLock: {
+      defaultValue: 'Caps Lock',
+      alternateValue: 'Caps Lock',
+      type: 'control',
+    },
+    KeyA: {
+      defaultValue: 'ф',
+      alternateValue: 'Ф',
+      type: 'letter',
+    },
+    KeyS: {
+      defaultValue: 'і',
+      alternateValue: 'І',
+      type: 'letter',
+    },
+    KeyD: {
+      defaultValue: 'в',
+      alternateValue: 'В',
+      type: 'letter',
+    },
+    KeyF: {
+      defaultValue: 'а',
+      alternateValue: 'А',
+      type: 'letter',
+    },
+    KeyG: {
+      defaultValue: 'п',
+      alternateValue: 'П',
+      type: 'letter',
+    },
+    KeyH: {
+      defaultValue: 'р',
+      alternateValue: 'Р',
+      type: 'letter',
+    },
+    KeyJ: {
+      defaultValue: 'о',
+      alternateValue: 'О',
+      type: 'letter',
+    },
+    KeyK: {
+      defaultValue: 'л',
+      alternateValue: 'Л',
+      type: 'letter',
+    },
+    KeyL: {
+      defaultValue: 'д',
+      alternateValue: 'Д',
+      type: 'letter',
+    },
+    Semicolon: {
+      defaultValue: 'ж',
+      alternateValue: 'Ж',
+      type: 'letter',
+    },
+    Quote: {
+      defaultValue: 'є',
+      alternateValue: 'Є',
+      type: 'letter',
+    },
+    Backslash: {
+      defaultValue: '\\',
+      alternateValue: '/',
+      type: 'char',
+    },
+    ShiftLeft: {
+      defaultValue: 'Shift',
+      alternateValue: 'Shift',
+      type: 'control',
+    },
+    IntlBackslash: {
+      defaultValue: 'ґ',
+      alternateValue: 'Ґ',
+      type: 'letter',
+    },
+    KeyZ: {
+      defaultValue: 'я',
+      alternateValue: 'Я',
+      type: 'letter',
+    },
+    KeyX: {
+      defaultValue: 'ч',
+      alternateValue: 'Ч',
+      type: 'letter',
+    },
+    KeyC: {
+      defaultValue: 'с',
+      alternateValue: 'С',
+      type: 'letter',
+    },
+    KeyV: {
+      defaultValue: 'м',
+      alternateValue: 'М',
+      type: 'letter',
+    },
+    KeyB: {
+      defaultValue: 'и',
+      alternateValue: 'И',
+      type: 'letter',
+    },
+    KeyN: {
+      defaultValue: 'т',
+      alternateValue: 'Т',
+      type: 'letter',
+    },
+    KeyM: {
+      defaultValue: 'ь',
+      alternateValue: 'Ь',
+      type: 'letter',
+    },
+    Comma: {
+      defaultValue: 'б',
+      alternateValue: 'Б',
+      type: 'letter',
+    },
+    Period: {
+      defaultValue: 'ю',
+      alternateValue: 'Ю',
+      type: 'letter',
+    },
+    Slash: {
+      defaultValue: '.',
+      alternateValue: ',',
+      type: 'char',
+    },
+    ShiftRight: {
+      defaultValue: 'Shift',
+      alternateValue: 'Shift',
+      type: 'control',
+    },
+    ControlLeft: {
+      defaultValue: 'Ctrl',
+      alternateValue: 'Ctrl',
+      type: 'control',
+    },
+    MetaLeft: {
+      defaultValue: 'Win',
+      alternateValue: 'Win',
+      type: 'control',
+    },
+    AltLeft: {
+      defaultValue: 'Alt',
+      alternateValue: 'Alt',
+      type: 'control',
+    },
+    Space: {
+      defaultValue: ' ',
+      alternateValue: ' ',
+      type: 'char',
+    },
+    AltRight: {
+      defaultValue: 'AltGr',
+      alternateValue: 'AltGr',
+      type: 'control',
+    },
+    MetaRight: {
+      defaultValue: 'Win',
+      alternateValue: 'Win',
+      type: 'control',
+    },
+    ContextMenu: {
+      defaultValue: 'ContextMenu',
+      alternateValue: 'ContextMenu',
+      type: 'control',
+    },
+    ControlRight: {
+      defaultValue: 'Ctrl',
+      alternateValue: 'Ctrl',
+      type: 'control',
+    },
+    Escape: {
+      defaultValue: 'Escape',
+      alternateValue: 'Escape',
+      type: 'control',
+    },
+    ArrowUp: {
+      defaultValue: '🠙',
+      alternateValue: '🠙',
+      type: 'cursor',
+    },
+    ArrowDown: {
+      defaultValue: '🠛',
+      alternateValue: '🠛',
+      type: 'cursor',
+    },
+    ArrowLeft: {
+      defaultValue: '🠘',
+      alternateValue: '🠘',
+      type: 'cursor',
+    },
+    ArrowRight: {
+      defaultValue: '🠚',
+      alternateValue: '🠚',
+      type: 'cursor',
+    },
+    Delete: {
+      defaultValue: 'Del',
+      alternateValue: 'Del',
+      type: 'control',
+    },
+    LangSwitch: {
+      defaultValue: '🌐',
+      alternateValue: '🌐',
+      type: 'control',
+    },
+  },
+};
+
+export default uk;
